258bb098bc57dd69057e094176b5112650cb8db5
bohokasmakkara <162553789+bohokasmakkara@users.noreply.github.com>
2024-03-07 14:47:41 +0200

    Update ft_range.c

--- ft_range.c
+++ ft_range.c
@@ -22,3 +22,22 @@ int	*ft_range(int min, int max)
 	}
 	return (array);
 }
+int	main(void)
+{
+	int	min;
+	int	max;
+	int	*tab;
+	int	i;
+	int	size;
+
+	i = 0;
+	min = 5;
+	max = 10;
+	size = max - min;
+	tab = ft_range(min, max);
+	while (i < size)
+	{
+		printf("%d", tab[i]);
+		i++;
+	}
+}
