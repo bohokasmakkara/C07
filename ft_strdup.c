f36c0fa0ae76da90676b908ff1e7d3ca937e9718
bohokasmakkara <162553789+bohokasmakkara@users.noreply.github.com>
2024-03-07 14:41:44 +0200

    Create ft_strdup.c
    The strdup() function allocates sufficient memory for a copy of the string s1, does the copy, and returns a pointer to it.  The pointer may
    subsequently be used as an argument to the function free(3).

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = (char *) malloc ((ft_strlen(src) + 1) * sizeof(char));
+	if (!dest)
+		return (NULL);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
