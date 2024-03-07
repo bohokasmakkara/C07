b8cb7838653a0cb240b8e2a7d05dc11947a0fdfc
bohokasmakkara <162553789+bohokasmakkara@users.noreply.github.com>
2024-03-07 14:46:28 +0200

    Create ft_strjoin.c
    THIS CODE GAVE SEGMENTATION FAULT
    • Write a function that will concatenate all the strings pointed by strs separated by
    sep.
    • size is the number of strings in strs
    • if size is 0, you must return an empty string that you can free().

---
+++ ft_strjoin.c
@@ -0,0 +1,77 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int	str_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*create_str(int size, char **strs, char *sep)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	len = 0;
+	i = 0;
+	while (i < size)
+	{
+		len += str_len(strs[i]);
+		i++;
+	}
+	len += str_len(sep) * (size - 1);
+	if (size <= 0)
+		len = 1;
+	str = (char *)malloc(sizeof(char) * (len +1));
+	if (!str)
+		return (NULL);
+	return (str);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*str;
+
+	str = create_str(size, strs, sep);
+	i = 0;
+	k = 0;
+	while (i++ < size)
+	{
+		j = 0;
+		while (strs[i][j])
+		{
+			str[k++] = strs[i][j];
+			j++;
+		}
+		j = 0;
+		while (sep[j] && i != size - 1)
+		{
+			str[k++] = sep[j];
+			j++;
+		}
+	}
+	str[k] = '\0';
+	return (str);
+}
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+ 	char *strs[] = {"Magyar", "orszag", "szep"};
+	char *sep = "+++";
+	int size = sizeof(strs) / sizeof(strs[0]);
+	char *result = ft_strjoin(size, strs, sep);
+
+	printf("%s\n", result);
+
+	free(result);
+	return 0;
+}
